796a1f9d0f7ea0d4ca02d6383fdf3e7a2df3cc28
fahruraji <owatanabe82@gmail.com>
2021-04-03 07:35:10 +0800

    Buat File Input Output

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+	public static void tampilanInfo() {
+		System.out.println();
+		System.out.println("== Data Mahasiswa ==================");
+		System.out.println("NPM  : 19620940");
+		System.out.println("Nama : Fahruraji");
+		System.out.println("====================================");
+	}
+}
